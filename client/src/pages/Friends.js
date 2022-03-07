3975cc1d77ea5728b3e78e29fde691fa5629b713
yoricktf <yorick.tenfeld@gmail.com>
2022-03-07 17:51:04 +0100

    problems with getting user details

--- client/src/pages/Friends.js
+++ client/src/pages/Friends.js
@@ -5,23 +5,40 @@ import { AuthContext } from '../context/auth'
 const Friends = () => {
   const [name, setName] = useState('');
   const [friends, setFriends] = useState('');
+  const [usersFriends, setUsersFriends] = useState([]);
   const { user } = useContext(AuthContext);
+  const storedToken = localStorage.getItem('authToken')
   let searchResult;
 
   const onChange = (e) => {
     setName(e.target.value)
   }
 
+  console.log(user)
 
-  const addFriend = e => {
-    console.log(user)
-    // user.friends.push(searchResult);
-    axios.post('/friends/addFriend', { searchResult, user })
-      .then(friend => {
+  const currentUser = () => {
+    axios.post('/friends/user', user)
+      .then(response => {
+        console.log(response);
       })
   }
 
   useEffect(() => {
+    currentUser()
+  }, [])
+
+
+  const addFriend = () => {
+    // console.log(user)
+    axios.post('/friends/addFriend', { searchResult, user })
+      .then(usersFriends => {
+        // console.log(updatedUser)
+        setUsersFriends(usersFriends.data)
+      })
+  }
+
+  useEffect(() => {
+
     axios.get('/friends')
       .then(allFriends => {
         // console.log(allFriends)
@@ -30,14 +47,12 @@ const Friends = () => {
       .catch(err => console.log(err))
   }, [])
 
-  // console.log(friends)
 
   for (let friend of friends) {
     if (friend.name === name) {
       searchResult = friend
     }
   }
-  // console.log(searchResult);
 
 
 
@@ -45,21 +60,23 @@ const Friends = () => {
     <>
       <div>Friends</div>
       <input type="text" value={name} onChange={onChange} />
-
-
       {searchResult ?
         (
           <div className="userCard">
             <h1>{searchResult.name}</h1>
             <button onClick={addFriend}>add as friend</button>
           </div>
-
-
         ) : (
           <h1></h1>
         )
-
       }
+      <ul>
+        {/* {usersFriends.map((friend) => (
+          <li>{friend}</li>
+
+        ))} */}
+
+      </ul>
     </>
   )
 }
--- routes/friend.routes.js
+++ routes/friend.routes.js
@@ -10,14 +10,28 @@ router.get('/', (req, res, next) => {
     .catch(err => next(err))
 })
 
+router.post('/user', (req, res, next) => {
+  console.log('this one', req.body)
+
+  User.findById(req.body._id)
+    .populate('friends')
+    .then(friends => {
+      console.log('working????????', friends)
+      res.status(200).json(friends)
+    })
+})
+
+
 router.post('/addFriend', (req, res, next) => {
   const searchResult = req.body.searchResult
   const currentUserId = req.body.user._id
-  User.findByIdAndUpdate(currentUserId)
+  User.findByIdAndUpdate(currentUserId, { $push: { friends: searchResult } }, { new: true })
     .then(user => {
-      user.friends.push(searchResult)
-      console.log(user)
+
+      res.status(200).json(user.friends)
+      // console.log(user)
     })
+    .catch(err => next(err))
   // console.log('add friend route triggered')
 })
 
